3fc8de545727325be591108a20c94ef3b3a53b92
kdanna <kayce.danna@gmail.com>
2016-08-26 17:45:12 -0600

    API GET route is returning json from seed data

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use('/', express.static('public'));
 
 
 //API routes
-//GET route to show all gardens
+//GET route to show all gardens.  "gardenModel is pulling from the garden.js export"
 app.get('/api/gardens', function (req, res){
-  db.Garden.find({}, function(err, gardens) {
+  db.gardenModel.find({}, function(err, gardens) {
   res.json(gardens);
 });
 });
