9060b9690420ef55a7214f63cb7a625c52aa5814
kdanna <kayce.danna@gmail.com>
2016-08-30 11:21:29 -0600

    seeding more data

--- app/db/seed.js
+++ app/db/seed.js
@@ -14,7 +14,7 @@ var plantsSeed = [
 			qty_planted: 10,
 			date_planted: '2016-05-25',
 			harvest_dates:['2016-09-01', '2016-08-25', '2016-08-15'],
-			harvest_qty:[5, 6, 10]
+			harvest_qty:[10, 6, 10]
 		},
 		{	
 			plant_type: 'Swiss Chard', 
@@ -23,6 +23,38 @@ var plantsSeed = [
 			date_planted: '2016-05-26',
 			harvest_dates:['2016-05-25', '2016-05-25', '2016-05-25'],
 			harvest_qty:[2, 5, 8]
+		},
+		{	
+			plant_type: 'Spinach', 
+			plant_variety: 'Dwarf',
+			qty_planted: 5,
+			date_planted: '2016-04-26',
+			harvest_dates:['2016-05-25', '2016-05-26', '2016-05-27'],
+			harvest_qty:[10, 5, 8]
+		},
+		{	
+			plant_type: 'Pepper',
+			plant_variety: 'Bell',
+			qty_planted: 8,
+			date_planted: '2016-05-26',
+			harvest_dates:['2016-08-25', '2016-08-29', '2016-09-01'],
+			harvest_qty:[2, 5, 8]
+		},
+		{	
+			plant_type: 'Pepper', 
+			plant_variety: 'Jalapeno',
+			qty_planted: 7,
+			date_planted: '2016-05-26',
+			harvest_dates:['2016-09-01', '2016-09-04', '2016-09-05'],
+			harvest_qty:[2, 9, 11]
+		},
+		{	
+			plant_type: 'Cucumber', 
+			plant_variety: 'Pickling',
+			qty_planted: 4,
+			date_planted: '2016-06-01',
+			harvest_dates:['2016-07-30', '2016-08-04', '2016-08-05'],
+			harvest_qty:[2, 9, 11]
 		}
 ];
 
